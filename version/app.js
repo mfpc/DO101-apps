52a2a13819f8559461ce038891c9f5cd14e437eb
Marcell Cruz <mfp.c@hotmail.com>
2020-09-05 11:30:05 -0300

    commit

--- version/app.js
+++ version/app.js
@@ -5,10 +5,19 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'This is version 1 of the app.' + '\n';
+    response = 'This is version 2 of the app.' + '\n';
 
     //send the response to the client
     res.send(response);
+
+});
+
+app.get('/mars', function (req, res) {
+
+  response = 'There is a planet called mars' + '\n';
+
+  //send the response to the client
+  res.send(response);
 
 });
 
